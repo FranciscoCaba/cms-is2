72e60d3e4274b02b4a534ac541ce99cbb9b1f54c
FranciscoCaba <francorv2@fpuna.edu.py>
2023-08-24 22:13:47 -0400

    bootstrap anhadido

--- cms_is2/settings.py
+++ cms_is2/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cms',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '',
     }
