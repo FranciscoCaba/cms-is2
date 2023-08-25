73f41e0f2831f837483debdb7b8f102ab11b451f
osmanimestre <manimestre018@fpuna.edu.py>
2023-08-24 23:39:18 -0400

    Autoasignacion Grupo

---
+++ accounts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.core.exceptions import ObjectDoesNotExist
+
+
+#perfil de usuario
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name='Usuario')
+
+    class Meta:
+        verbose_name = 'perfil'
+        verbose_name_plural = 'perfiles'
+        ordering = ['-id']    
+    
+    
+    def __str__(self):
+        return self.user.username
+    
+def create_user_profile(sender, instance, created, **kwargs):
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
