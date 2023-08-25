2bf242cdd21527958b66b812199c266560c7f081
tobias <otazo914@fpuna.edu.py>
2023-08-25 00:59:02 -0400

    Perfil

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from.views import ProfileView
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView
+from django.contrib.auth.models import Group
 
 # Create your views here.
 def index(request):
     return render(request, 'vistas/inicio.html')
+
+class CustomTemplateView(TemplateView):
+    group_name= None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user=self.request.user
+        if user.is_authenticated:
+            group= Group.objects.filter(user=user).first()
+            if group:
+                self.group_name=group.name
+        context['group_name'] = self.group_name
+        return context
+    
+class ProfileView(CustomTemplateView):
+    template_name= 'profile/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        user=self.request.user
+        return context
