3a9dd7c7df835a9233c1bb483d3c31372f047450
osmanimestre <manimestre018@fpuna.edu.py>
2023-09-06 22:59:37 -0400

    cambio idioma url a espanhol

--- contenido/urls.py
+++ contenido/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 from . import views
 urlpatterns = [
-    path('create', login_required(views.ContenidoFormView.as_view()) , name='create'),
-    path('category/create', login_required(views.CategoriaFormView.as_view()), name='category/create'),
+    path('crear', login_required(views.ContenidoFormView.as_view()) , name='crear'),
+    path('categoria/crear', login_required(views.CategoriaFormView.as_view()), name='categoria/crear'),
 ]
