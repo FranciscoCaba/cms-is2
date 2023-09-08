f8b9a5caf3d1326392443b51aef0f06296263491
derliscaceres <derliscaceresn@fpuna.edu.py>
2023-09-08 00:03:35 -0400

    configuracion correcta de ambiente de produccion

--- cms_is2/produccion.py
+++ cms_is2/produccion.py
@@ -2,7 +2,7 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
