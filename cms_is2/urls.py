ecece266b95455b13595d0d73f8eaa640ea2312b
derliscaceres <derliscaceresn@fpuna.edu.py>
2023-09-08 00:03:09 -0400

    agregado soporte error 404

--- cms_is2/urls.py
+++ cms_is2/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from app.views import PaginaNoEncontradaView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('contenido/', include('contenido.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^.*/$', PaginaNoEncontradaView.as_view(), name='pagina_no_encontrada'),
 ]
