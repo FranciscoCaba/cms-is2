fb29e38a62a1d64fb59fb041826fea181f03e263
derliscaceres <derliscaceresn@fpuna.edu.py>
2023-11-02 22:40:42 -0300

    intento de deploy xd

---
+++ deploy/bdd_crear.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Variables
+db_name="cms"
+db_user="cmsadmin"
+db_password="cmsadmin"
+
+sudo su postgres <<EOF
+echo 'Creando base de datos $db_name ...'
+psql -c "CREATE DATABASE $db_name;"
+
+echo 'Creando usuario $db_user con contrasenha $db_password ...'
+psql -c "CREATE USER $db_user WITH PASSWORD '$db_password';"
+
+echo 'Dando privilegios al usuario sobre la base de datos...'
+psql -c "GRANT ALL PRIVILEGES ON DATABASE $db_name TO $db_user;"
+EOF
